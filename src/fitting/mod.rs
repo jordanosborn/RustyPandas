51d6d4e221853b3d1c93e740847dee36e584f83f
Jordan Osborn <jordanosborn0@gmail.com>
2019-06-18 16:20:11 +0100

    added fit_weighted

--- src/fitting/mod.rs
+++ src/fitting/mod.rs
@@ -4,6 +4,7 @@ use crate::calculus::differentiation::*;
 use crate::Function;
 use rayon::prelude::*;
 type Cost = fn(Function, Vec<f64>, Vec<Vec<f64>>, Vec<f64>) -> f64;
+type CostWeighted = fn(Function, Vec<f64>, Vec<Vec<f64>>, Vec<f64>, Vec<f64>) -> f64;
 
 pub fn least_squares(f: Function, params: Vec<f64>, vars: Vec<Vec<f64>>, y: Vec<f64>) -> f64 {
     vars.par_iter()
@@ -23,6 +24,23 @@ pub fn weighted_least_squares(
         .zip(weights.par_iter())
         .map(|((x, y), w)| w * (y - f(x, &params).powf(2.0)))
         .sum()
+}
+pub fn fit_weighted(
+    f: Function,
+    vars: Vec<Vec<f64>>,
+    y: Vec<f64>,
+    weights: Vec<f64>,
+    bounds: Vec<(f64, f64)>,
+    cost_function: Option<CostWeighted>,
+    threshold: Option<f64>,
+) {
+    let cost_function = if let Some(c) = cost_function {
+        c
+    } else {
+        weighted_least_squares
+    };
+    let cost_function = |(f, params, vars, y)| cost_function(f, params, vars, y, weights);
+    //Use diff to minimise cost
 }
 
 pub fn fit(
