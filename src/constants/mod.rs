020f256f5aa5e2aacfd166dcd7f996bab8553059
Jordan Osborn <jordanosborn0@gmail.com>
2019-06-22 15:14:56 +0100

    added all si prefixes

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,13 +1,27 @@
+#![allow(non_upper_case_globals)]
+
 pub static PI: f64 = std::f64::consts::PI;
 pub static E: f64 = std::f64::consts::E;
 
-#![allow(non_upper_case_globals)]
+//SI prefixes
+pub const Yotta: f64 = 1e24;
+pub const Zetta: f64 = 1e21;
+pub const Exa: f64 = 1e18;
+pub const Peta: f64 = 1e15;
 pub const Tera: f64 = 1e12;
 pub const Giga: f64 = 1e9;
 pub const Mega: f64 = 1e6;
 pub const Kilo: f64 = 1e3;
+pub const Hecto: f64 = 1e2;
+pub const Deca: f64 = 1e1;
 
+pub const Deci: f64 = 1e-1;
+pub const Centi: f64 = 1e-2;
 pub const Milli: f64 = 1e-3;
 pub const Micro: f64 = 1e-6;
 pub const Nano: f64 = 1e-9;
 pub const Pico: f64 = 1e-12;
+pub const Femto: f64 = 1e-15;
+pub const Atto: f64 = 1e-18;
+pub const Zepto: f64 = 1e-21;
+pub const Yocto: f64 = 1e-24;
