1abad231619dd10560f1aabe3667d143c5c6595c
Jordan Osborn <jordanosborn0@gmail.com>
2019-08-30 16:05:29 +0100

    series iloc functions

--- src/series/mod.rs
+++ src/series/mod.rs
@@ -1,7 +1,11 @@
 #![allow(dead_code)]
 use chrono::prelude::*;
 use std::any::Any;
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::iter::FromIterator;
 
+#[derive(Copy, Clone, Debug)]
 pub enum Dtype {
     STRING,
     FLOAT,
@@ -12,15 +16,91 @@ pub enum Dtype {
 #[allow(type_alias_bounds)]
 type UnderlyingStorage<T: Any> = Vec<T>;
 
-pub struct Series<T: Any + Clone, K: Any + Clone + From<usize>> {
+pub struct Series<T: Any + Clone, K: Any + Clone + From<usize> + std::cmp::Eq + Hash> {
     data: UnderlyingStorage<T>,
     pub dtype: Dtype,
-    pub index: UnderlyingStorage<K>,
+    pub index_array: UnderlyingStorage<K>,
     pub index_dtype: Dtype,
     pub name: String,
 }
 
-impl<T: Any + Clone, K: Any + Clone + From<usize>> Series<T, K> {
+impl<T: Any + Clone, K: Any + Clone + From<usize> + std::cmp::Eq + Hash> Series<T, K> {
+    fn iloc(&self, idx: &K) -> Option<T> {
+        let pos = self.index_array.iter().position(|x| *x == *idx);
+        if let Some(p) = pos {
+            Some(self.data[p].clone())
+        } else {
+            None
+        }
+    }
+
+    fn iloc_vec(&self, idx: &[K]) -> Option<Self> {
+        let pos = self
+            .index_array
+            .iter()
+            .enumerate()
+            .flat_map(|(i, x)| {
+                if idx.iter().find(|y| x == *y) == None {
+                    None
+                } else {
+                    Some(i)
+                }
+            })
+            .collect::<Vec<_>>();
+        if pos.len() == idx.len() {
+            Some(Self {
+                data: pos
+                    .iter()
+                    .map(|x| self.data[*x].clone())
+                    .collect::<UnderlyingStorage<T>>(),
+                dtype: self.dtype,
+                index_array: UnderlyingStorage::from(idx),
+                index_dtype: self.index_dtype,
+                name: self.name.clone(),
+            })
+        } else {
+            None
+        }
+    }
+
+    fn loc(&self, idx: &K) -> Option<T> {
+        let pos = self.index_array.iter().position(|x| *x == *idx);
+        if let Some(p) = pos {
+            Some(self.data[p].clone())
+        } else {
+            None
+        }
+    }
+
+    fn loc_vec(&self, idx: &[K]) -> Option<Self> {
+        let pos = self
+            .index_array
+            .iter()
+            .enumerate()
+            .flat_map(|(i, x)| {
+                if idx.iter().find(|y| x == *y) == None {
+                    None
+                } else {
+                    Some(i)
+                }
+            })
+            .collect::<Vec<_>>();
+        if pos.len() == idx.len() {
+            Some(Self {
+                data: pos
+                    .iter()
+                    .map(|x| self.data[*x].clone())
+                    .collect::<UnderlyingStorage<T>>(),
+                dtype: self.dtype,
+                index_array: UnderlyingStorage::from(idx),
+                index_dtype: self.index_dtype,
+                name: self.name.clone(),
+            })
+        } else {
+            None
+        }
+    }
+
     #[allow(clippy::redundant_closure, dead_code)]
     fn new(
         array: &[T],
@@ -30,20 +110,27 @@ impl<T: Any + Clone, K: Any + Clone + From<usize>> Series<T, K> {
     ) -> Option<Self> {
         let idx: UnderlyingStorage<K>;
         let idx_dtype: Dtype;
+        let length: usize;
         if let Some((i, d)) = index {
+            length = i.len();
             idx = UnderlyingStorage::from(i);
+            let hash_idx: std::collections::HashSet<K> = HashSet::from_iter(idx.clone());
+            if length != hash_idx.len() {
+                return None;
+            }
             idx_dtype = d;
         } else {
             idx = (0..array.len())
                 .map(|x: usize| K::from(x))
                 .collect::<UnderlyingStorage<K>>();
+            length = array.len();
             idx_dtype = Dtype::INTEGER;
         };
-        if idx.len() == array.len() {
+        if length == array.len() {
             Some(Self {
                 data: array.to_vec(),
                 dtype,
-                index: idx.to_vec(),
+                index_array: idx,
                 index_dtype: idx_dtype,
                 name: name.unwrap_or_else(|| String::from("")),
             })
@@ -52,3 +139,16 @@ impl<T: Any + Clone, K: Any + Clone + From<usize>> Series<T, K> {
         }
     }
 }
+
+// impl<T: Any + Clone, K: Any + Clone + From<usize> + Hash + std::cmp::Eq> std::ops::Index<K>  for Series<T, K> {
+//     type Output = Option<T>;
+//     fn index(&self, idx: K) -> &<Self as std::ops::Index<K>>::Output {
+//         let pos = self.index_array.iter().position(|x| *x == idx);
+//         if let Some(p) = pos {
+//             let out = self.data[p].clone();
+//             Some(out)
+//         } else {
+//             &None
+//         }
+//     }
+// }
