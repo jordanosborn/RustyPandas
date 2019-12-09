a280fb6bbbccfbc5c1f0232e92e0b9a6f9ae2825
Jordan Osborn <jordanosborn@users.noreply.github.com>
2019-12-09 14:59:24 +0000

    push

--- src/types/dual.rs
+++ src/types/dual.rs
@@ -7,12 +7,17 @@ pub struct Dual<T: Num + Copy> {
     pub b: T,
 }
 
-impl<T: Num + Copy + std::ops::Neg> std::fmt::Display for Dual<T> 
-where T: std::fmt::Display + PartialOrd,
-    <T as std::ops::Neg>::Output: std::fmt::Display {
+impl<T: Num + Copy + std::ops::Neg> std::fmt::Display for Dual<T>
+where
+    T: std::fmt::Display + PartialOrd,
+    <T as std::ops::Neg>::Output: std::fmt::Display,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
-            f, "{} {}e", self.a, if self.b < T::zero() {
+            f,
+            "{} {}e",
+            self.a,
+            if self.b < T::zero() {
                 format!["- {}", -self.b]
             } else {
                 format!["+ {}", self.b]
@@ -27,7 +32,40 @@ impl<T: Num + Copy> Dual<T> {
     }
 }
 
+impl<T: Num + Copy> From<T> for Dual<T> {
+    fn from(value: T) -> Self {
+        Self {
+            a: value,
+            b: T::zero(),
+        }
+    }
+}
+
 impl<T: Float + Copy> Dual<T> {
+    #[inline]
+    fn exp(self) -> Self {
+        Self {
+            a: self.a.exp(),
+            b: self.b * self.a.exp(),
+        }
+    }
+
+    #[inline]
+    fn powf(self, other: T) -> Self {
+        Self {
+            a: self.a.powf(other),
+            b: self.b * other * self.a.powf(other - T::one()),
+        }
+    }
+
+    // #[inline]
+    // fn pow(self, other: Dual<T>) -> Self {
+    //     Self {
+    //         a:
+    //     }
+    // }
+
+    #[inline]
     fn sin(self) -> Self {
         Self {
             a: self.a.sin(),
@@ -35,6 +73,15 @@ impl<T: Float + Copy> Dual<T> {
         }
     }
 
+    #[inline]
+    fn sinh(self) -> Self {
+        Self {
+            a: self.a.sinh(),
+            b: self.b * self.a.cosh(),
+        }
+    }
+
+    #[inline]
     fn cos(self) -> Self {
         Self {
             a: self.a.cos(),
@@ -42,10 +89,27 @@ impl<T: Float + Copy> Dual<T> {
         }
     }
 
+    #[inline]
+    fn cosh(self) -> Self {
+        Self {
+            a: self.a.cosh(),
+            b: self.b * self.a.sinh(),
+        }
+    }
+
+    #[inline]
     fn tan(self) -> Self {
         Self {
             a: self.a.tan(),
             b: self.b * self.a.cos().powf(T::from(-2.0_f64).unwrap()),
+        }
+    }
+
+    #[inline]
+    fn tanh(self) -> Self {
+        Self {
+            a: self.a.tanh(),
+            b: self.b * (T::one() - self.a.tanh().powf(T::from(2.0_f64).unwrap())),
         }
     }
 }
@@ -53,6 +117,7 @@ impl<T: Float + Copy> Dual<T> {
 impl<T: Num + Copy> std::ops::Add for Dual<T> {
     type Output = Dual<T>;
 
+    #[inline]
     fn add(self, other: Dual<T>) -> Self::Output {
         Self::Output {
             a: self.a + other.a,
@@ -64,6 +129,7 @@ impl<T: Num + Copy> std::ops::Add for Dual<T> {
 impl<T: Num + Copy> std::ops::Sub for Dual<T> {
     type Output = Dual<T>;
 
+    #[inline]
     fn sub(self, other: Dual<T>) -> Self::Output {
         Self::Output {
             a: self.a - other.a,
@@ -75,6 +141,7 @@ impl<T: Num + Copy> std::ops::Sub for Dual<T> {
 impl<T: Num + Copy> std::ops::Mul for Dual<T> {
     type Output = Dual<T>;
 
+    #[inline]
     fn mul(self, other: Dual<T>) -> Self::Output {
         Self::Output {
             a: self.a * other.a,
@@ -85,11 +152,42 @@ impl<T: Num + Copy> std::ops::Mul for Dual<T> {
 
 impl<T: Num + Copy + std::ops::Neg<Output = T>> std::ops::Neg for Dual<T> {
     type Output = Dual<T>;
-
+    #[inline]
     fn neg(self) -> Self::Output {
         Self::Output {
             a: -self.a,
             b: -self.b,
+        }
+    }
+}
+
+impl<T: Num + Copy, V: Num + Copy> std::ops::Mul<V> for Dual<T>
+where
+    T: std::ops::Mul<V, Output = T>,
+{
+    type Output = Dual<T>;
+    #[inline]
+    fn mul(self, other: V) -> Self::Output {
+        Self::Output {
+            a: self.a * other,
+            b: self.b * other,
+        }
+    }
+}
+
+/*
+    Symmetric lhs and rhs multiplication only implemented for f64...
+*/
+impl<T: Num + Copy> std::ops::Mul<Dual<T>> for f64
+where
+    T: std::ops::Mul<f64, Output = T>,
+{
+    type Output = Dual<T>;
+    #[inline]
+    fn mul(self, other: Dual<T>) -> Self::Output {
+        Self::Output {
+            a: other.a * self,
+            b: other.b * self,
         }
     }
 }
@@ -99,7 +197,7 @@ where
     T: std::ops::Div<V, Output = T>,
 {
     type Output = Dual<T>;
-
+    #[inline]
     fn div(self, other: V) -> Self::Output {
         Self::Output {
             a: self.a / other,
@@ -112,6 +210,7 @@ impl<T: Num + Copy + std::ops::Neg<Output = T>> std::ops::Div for Dual<T> {
     type Output = Dual<T>;
 
     #[allow(clippy::suspicious_arithmetic_impl)]
+    #[inline]
     fn div(self, other: Dual<T>) -> Self::Output {
         let divisor = self.a * other.a;
         Self::Output {
@@ -146,7 +245,7 @@ mod tests {
         }
 
         fn dcos_x_y_dy(x: f64, y: f64) -> f64 {
-            - x * (x * y).sin()
+            -x * (x * y).sin()
         }
 
         let d1 = Dual::new(5.0, 0.0);
